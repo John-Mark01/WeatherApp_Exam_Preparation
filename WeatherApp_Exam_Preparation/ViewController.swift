4510833ec71f8c45235c993bd01d739023d082b5
John-Mark Iliev <147177515+John-Mark01@users.noreply.github.com>
2024-02-01 10:32:15 +0200

    Initial Commit

---
+++ WeatherApp_Exam_Preparation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_Exam_Preparation
+//
+//  Created by John-Mark Iliev on 1.02.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
